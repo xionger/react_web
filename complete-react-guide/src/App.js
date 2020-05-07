b2b94fcef4eec7b88314a8339de99cbec050df72
Xunhao Xiong <xxiong42@gatech.edu>
2020-05-06 23:38:59 -0500

    3.42 handle event

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -10,11 +10,19 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    this.setState({persons: [
+      {name: "Maxwell", age: 28},
+      {name: "Joe", age: 18}
+    ]});
+  }
+  
   render(){
       return (
         <div className="App">
           <header className="App-header">
             <p>Hello, React!</p>
+            <button onClick={this.switchNameHandler}>Click Me!</button>
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
             <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
           </header>
