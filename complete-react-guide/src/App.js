6e143b0b40d42ed4d116f88dc38f79a1d352e0b0
Xunhao Xiong <xxiong42@gatech.edu>
2020-05-07 00:06:16 -0500

    3.44 Hooks useState

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -1,34 +1,32 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personState, setPersonState] = useState({
     persons: [
       {name: "Max", age: 28},
       {name: "Joe", age: 18}
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    this.setState({persons: [
+  const switchNameHandler = () => {
+    setPersonState({persons: [
       {name: "Maxwell", age: 28},
       {name: "Joe", age: 18}
     ]});
   }
-  
-  render(){
-      return (
-        <div className="App">
-          <header className="App-header">
-            <p>Hello, React!</p>
-            <button onClick={this.switchNameHandler}>Click Me!</button>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-          </header>
-        </div>
-      )
-    }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello, React!</p>
+        <button onClick={switchNameHandler}>Click Me!</button>
+        <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+        <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+      </header>
+    </div>
+  )
 }
 
 export default App;
