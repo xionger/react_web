d78b2eae8c6094ca67314f00e1908342d9617895
Xunhao Xiong <xxiong42@gatech.edu>
2020-05-07 23:49:46 -0500

    5.68 using Radium

--- complete-react-guide/src/App.js
+++ complete-react-guide/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -44,7 +45,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -64,7 +69,12 @@ class App extends Component {
           }
         </div>
       );
+
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'pink',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -86,4 +96,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
