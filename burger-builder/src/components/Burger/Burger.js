2f1e353dce8a3ea304818cb3a9483a10270a8d0d
Xunhao Xiong <xxiong42@gatech.edu>
2020-05-09 11:22:30 -0500

    8.130 calculate sum of ingredients

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const tsIngredients = Object.keys(props.ingredients)
+    let tsIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        }).reduce((arr, e) => {
+            return arr.concat(e)
+        }, []);
+
+        if(!tsIngredients.length) {
+            tsIngredients = <p>Please start adding some ingredients.</p>
+        }
+
     return (
         <div className="Burger">
             <BugerIngredient type="bread-top" />
