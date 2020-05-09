4725d82521d99ab201c11e6f14d5cfce69ea0ef6
Xunhao Xiong <xxiong42@gatech.edu>
2020-05-09 00:47:58 -0500

    8.124 add Layout component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <div>
+      <Layout>Some contents here!</Layout>
     </div>
   );
 }
