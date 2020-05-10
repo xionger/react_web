0e5cef579e42264608a98c7c434f7df9fd8d8d76
Xunhao Xiong <xxiong42@gatech.edu>
2020-05-10 16:28:16 -0500

    8.132 output  BuildControls

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
 import './BuildControls.css';
 
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Meat', type: 'meat' },
+    { label: 'Cheese', type: 'cheese' }
+];
 const buildControls = (props) => {
     return (
-        <div></div>
+        <div className="BuildControls">
+            {controls.map(ct => (
+                <BuildControl key={ct.label} label={ct.label} />
+            ))}
+        </div>
     );
 }
 
--- burger-builder/src/containers/BurgerBuilder/BuigerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BuigerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Aux';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
@@ -17,7 +18,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Builder Control</div>
+                <BuildControls />
             </Aux>
         );
     }
