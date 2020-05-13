8512151b6a76e7762a9de8f712202ecae597482b
Xunhao Xiong <xxiong42@gatech.edu>
2020-05-12 20:28:20 -0500

    8.146 add sidedrawer

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className="Content">
             {props.children}
         </main>
